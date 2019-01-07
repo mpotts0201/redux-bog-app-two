2ed9ed13c726e79b7dcf0f3fbf9095624c68ef92
mpotts0201 <mpotts0201@gmail.com>
2019-01-07 17:01:59 -0500

    Seeded

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
+
+  has_many :creatures, dependent: :destroy
+
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+
+murphy = User.create(email: 'murphy@potts.com', password: 'password', password_confirmation: 'password')
+
+kate = User.create(email: 'kate@kate.com', password: 'password', password_confirmation: 'password')
+
+yoda = Creature.create(name: 'Yoda', description: 'Yo yo yoda', user_id: murphy.id)
+chewy = Creature.create(name: 'Chewy', description: 'Ch ch chewy', user_id: murphy.id)
+r2 = Creature.create(name: 'R2', description: 'R R R2', user_id: murphy.id)
+
+
+pikachu = Creature.create(name: 'Pikachu', description: 'Peeka', user_id: kate.id)
+charzard = Creature.create(name: 'Charzard', description: 'Flamey', user_id: kate.id)
+shmoda = Creature.create(name: 'Shmoda', description: 'Not yoda', user_id: kate.id)
+
